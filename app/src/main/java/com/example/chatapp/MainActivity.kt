f6332470e8cf44eb8070dab41b43cb61ed2f737e
Ayush <turakayush@gmail.com>
2024-02-08 20:17:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.chatapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+class MainActivity : AppCompatActivity() {
+private lateinit var reView: RecyclerView
+private lateinit var userList:ArrayList<User>
+private lateinit var adapter:UserAdapter
+private lateinit var mAuth:FirebaseAuth
+private lateinit var mDbRef : DatabaseReference
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+         userList = ArrayList()
+         adapter = UserAdapter(this,userList)
+
+         mAuth = FirebaseAuth.getInstance()
+         mDbRef = FirebaseDatabase.getInstance().getReference()
+
+        reView = findViewById(R.id.rv)
+        reView.layoutManager = LinearLayoutManager(this)
+        reView.adapter = adapter
+
+        mDbRef.child("user").addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                userList.clear()
+                for(postSnapshot in snapshot.children){
+                   val currentUser = postSnapshot.getValue(User::class.java)
+                    if(mAuth.currentUser?.uid != currentUser?.uid){
+                        userList.add(currentUser!!)
+                    }
+
+                }
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.logOut){
+            mAuth.signOut()
+            val intent = Intent(this@MainActivity,Login::class.java)
+            finish()
+            startActivity(intent)
+            return true
+        }
+        return true
+    }
+}
